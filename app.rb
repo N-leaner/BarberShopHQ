c721df61b23a795ed6db282eac1b7e3871df17d4
N-leaner <yahoo@pizd.ec>
2016-07-08 16:56:56 +0300

    Add Jqwery Bootstrap

--- app.rb
+++ app.rb
@@ -15,7 +15,34 @@ class Barber < ActiveRecord::Base
 end	
 
 get '/' do	
-
 	@barbers = Barber.all
 	erb :index
+end
+
+get '/visit'do
+	@barbers = Barber.order 'name desc'
+	erb :visit
+end
+
+post '/visit' do
+	@barbers = Barber.order 'name desc'
+	@master = params[:master]
+	@user_name = params[:username].strip.capitalize
+	@user_phone = params[:user_telephone].strip
+
+	@error = ''
+	if @user_name == ''
+		@error = 'Enter a name!'		
+	end
+	if @user_phone == ''
+		@error = 'Enter a phone!'		
+	end	
+	if !@error || @error.length == 0		
+		cl = Client.new
+		cl.name = @user_name
+		cl.phone = @user_phone
+		cl.save
+	end	
+
+	erb :visit
 end
