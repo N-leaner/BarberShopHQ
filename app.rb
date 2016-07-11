c79a667348732c52e293f81cb3fffde4b10d81f2
N-leaner <yahoo@pizd.ec>
2016-07-11 11:36:38 +0300

    Save client information the cool way

--- app.rb
+++ app.rb
@@ -33,34 +33,8 @@ get '/contacts' do
 end
 
 post '/visit' do
-	@barbers = Barber.order 'name desc'
-	@master = params[:master]
-	@user_name = params[:username].strip.capitalize
-	@user_phone = params[:user_telephone].strip
-	@color 		= params[:color].strip
-	@date_visit = params[:date_].strip
-
-	hh_ver = {:username => 'Не указано имя',
-			:master => 'Не указан мастер',
-			:date_ => 'Не указана дата'}
-
-	@error = hh_ver.select {|key,_| params[key] == ''}.values.join(", ")		
-
-	if @error == ''
-		cl = Client.new
-		cl.name = @user_name
-		cl.phone = @user_phone
-		cl.datestamp = @date_visit
-		cl.color = @color
-		cl.barber = @master
-		cl.save
-		
-		@user_name = ''
-		@user_phone = ''
-		@color 		= params[:color].strip
-		@date_visit = ''
-		@done = 'You visit save successfully!'
-	end	
+	c = Client.new params[:client]
+	c.save
 
 	erb :visit
 end
